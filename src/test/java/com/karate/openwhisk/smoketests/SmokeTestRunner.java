d3abffdb3ff0d2d98e5292e33470c30ba89ac838
rahultripathi <rtripath@adobe.com>
2018-06-12 09:33:22 -0700

    Added Cucumber Report

--- src/test/java/com/karate/openwhisk/smoketests/SmokeTestRunner.java
+++ src/test/java/com/karate/openwhisk/smoketests/SmokeTestRunner.java
@@ -4,8 +4,6 @@
 package com.karate.openwhisk.smoketests;
 import com.intuit.karate.cucumber.CucumberRunner;
 import com.intuit.karate.cucumber.KarateStats;
-import com.intuit.karate.junit4.Karate;
-
 import cucumber.api.CucumberOptions;
 import java.io.File;
 import java.util.ArrayList;
@@ -16,8 +14,6 @@ import net.masterthought.cucumber.ReportBuilder;
 import org.apache.commons.io.FileUtils;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-
 import static org.junit.Assert.*;	
 
 /**
@@ -25,13 +21,25 @@ import static org.junit.Assert.*;
  *
  * 
  */
-@RunWith(Karate.class)
+
 @CucumberOptions(tags = {"~@ignore","~@driver","~@reliability","~@resiliency","~@concurrent","~@wskfunctions"})
 public class SmokeTestRunner {
-	
+	  @Test
+	    public void testwskFunctions() {
+	      
+		    String karateOutputPath = "target/surefire-reports";
+	        KarateStats stats = CucumberRunner.parallel(getClass(), 5, karateOutputPath);
+	        generateReport(karateOutputPath);
+	        assertTrue("there are scenario failures", stats.getFailCount() == 0);        
+	    }
+	    private static void generateReport(String karateOutputPath) {
+	        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
+	        List<String> jsonPaths = new ArrayList(jsonFiles.size());
+	        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+	        Configuration config = new Configuration(new File("target"), "openwhisk");
+	        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+	        reportBuilder.generateReports();        
+	    }
 	  
 	  
 }
-
-
-
